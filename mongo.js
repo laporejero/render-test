33346f5836f1ca64d58aa28944e7c8c828bb90f8
laporejero <laporejero1297@gmail.com>
2026-06-06 15:17:00 +0800

    added cloud-based MongoDB database

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = encodeURIComponent(process.argv[2])
+
+const url = `mongodb://fullstack:${password}@ac-umezng9-shard-00-00.ms8yls1.mongodb.net:27017,ac-umezng9-shard-00-01.ms8yls1.mongodb.net:27017,ac-umezng9-shard-00-02.ms8yls1.mongodb.net:27017/?tls=true&replicaSet=atlas-9fcvkn-shard-0&authSource=admin&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// Generate new note
+// const note = new Note({
+//   content: 'GET and POST are the most important methods of HTTP protocol',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+// Fetching objects from the database
+Note.find({ important: false }).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
