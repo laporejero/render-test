22ff524aa4c6a1e1e9e2743c757a3b3be1108b70
laporejero <laporejero1297@gmail.com>
2026-06-12 00:34:42 +0800

    updated project structure and added tests

--- models/note.js
+++ models/note.js
@@ -1,18 +1,7 @@
 const mongoose = require('mongoose')
+const config = require('../utils/config')
 
 mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose
-  .connect(url, { family: 4 })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
