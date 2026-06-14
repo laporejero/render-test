132b7bc1a3e48eb50b9850b895c1e2a562718a76
laporejero <laporejero1297@gmail.com>
2026-06-14 23:15:42 +0800

    refactored backend to async/await syntax by testing first

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = encodeURIComponent(process.argv[2])
 
-const url = `mongodb://fullstack:${password}@ac-umezng9-shard-00-00.ms8yls1.mongodb.net:27017,ac-umezng9-shard-00-01.ms8yls1.mongodb.net:27017,ac-umezng9-shard-00-02.ms8yls1.mongodb.net:27017/noteApp?tls=true&replicaSet=atlas-9fcvkn-shard-0&authSource=admin&appName=Cluster0`
+const url = `mongodb://fullstack:${password}@ac-umezng9-shard-00-00.ms8yls1.mongodb.net:27017,ac-umezng9-shard-00-01.ms8yls1.mongodb.net:27017,ac-umezng9-shard-00-02.ms8yls1.mongodb.net:27017/testNoteApp?tls=true&replicaSet=atlas-9fcvkn-shard-0&authSource=admin&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -25,20 +25,20 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 // Generate new note
-// const note = new Note({
-//   content: 'abc',
-//   important: true,
-// })
+const note = new Note({
+  content: 'CSS is hard',
+  important: true,
+})
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-
-// Fetching objects from the database
-Note.find({ important: false }).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+note.save().then(result => {
+  console.log('note saved!')
   mongoose.connection.close()
 })
+
+// Fetching objects from the database
+// Note.find({ important: false }).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
